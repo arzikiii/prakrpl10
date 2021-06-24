435a44716f1d278e29b1df64a46c6f625ad2b77a
arzikiii <arzi1605@gmail.com>
2021-06-24 22:54:02 +0900

    first commit

---
+++ app/src/main/java/com/finalproject/queue/adminFragment/AdminHomeFragment.kt
@@ -0,0 +1,36 @@
+package com.finalproject.queue.adminFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.finalproject.queue.R
+import com.finalproject.queue.databinding.FragmentAdminHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AdminHomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AdminHomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentAdminHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_admin_home, container, false)
+        binding.logout.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_adminHomeFragment_to_loginFragment)
+        }
+        binding.create.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_adminHomeFragment_to_createQueueFragment)
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/finalproject/queue/userFragment/HomeFragment.kt
@@ -0,0 +1,66 @@
+package com.finalproject.queue.userFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.finalproject.queue.R
+import com.finalproject.queue.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.logout.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_loginFragment)
+        }
+        binding.antrianku.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_queueFragment)
+        }
+        binding.cari.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_searchFragment)
+        }
+        binding.riwayat.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_historyFragment)
+        }
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
